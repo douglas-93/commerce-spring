ecee4569d09c104ce36ab6f4e4e850dd8ff66176
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-12-29 15:29:07 -0300

    Association implementation

--- src/main/java/com/dolts/springcommerce/entities/Category.java
+++ src/main/java/com/dolts/springcommerce/entities/Category.java
@@ -1,5 +1,6 @@
 package com.dolts.springcommerce.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -16,8 +17,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    private Set<Product> products = new HashSet<>();
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
+    private final Set<Product> products = new HashSet<>();
 
     public Category() {
     }
